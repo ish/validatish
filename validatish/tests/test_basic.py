aa7fb7f0c9e7a94ef2d39d6decd5fba950db6ae6
Tim Parkin <tim.parkin@jdi-associates.com>
2008-11-30 19:11:59 +0000

    optimised tests to make more readable

--- validatish/tests/test_basic.py
+++ validatish/tests/test_basic.py
@@ -2,9 +2,37 @@ import unittest
 from validatish import validate
 
 
+def error_message(type,self,v,e):
+    if type == 'function':
+        return "'%s' secton of function '%s' on '%s' failed with %s"%(
+            self.section, self.type, v, e.msg)
+    if type == 'class':
+        return "'%s' secton of object '%s' on '%s' failed with %s"%(
+            self.section, self.type.lower(), v, e.msg)
+
+def check_pass(type, self, fn, values):
+    for v in values:
+        try:
+            # Attempt validation using function
+            fn(v)
+        except Exception, e:
+            self.fail(error_message(type,self,v,e))
+
+def check_fail(type, self, fn, values):
+    for v in values:
+        try:
+            fn(v)
+        except validate.Invalid, e:
+            continue
+        except Exception, e:
+            self.fail(error_message(type,self,v,e))
+
 class TestString(unittest.TestCase):
 
+    type='String'
+
     def test_validate_pass(self):
+        self.section='pass'
         values = [
             'foo',
             '1',
@@ -12,64 +40,55 @@ class TestString(unittest.TestCase):
             'sad',
             None,
             ]
-        for v in values:
-            print 'checking %s'%v
-            validate.string(v)
-        validator = validate.String()
-        for v in values:
-            print 'checking %s'%v
-            validator.validate(v)
-
+        fn = validate.string
+        check_pass('function',self, fn, values)
+        fn = validate.String().validate
+        check_pass('class', self, fn, values)
 
     def test_validate_fail(self):
+        self.section='pass'
         values = [
             1,
             1.01,
             ['a','b','c'],
             ['a'],
             ]
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validate.string, v)
-        validator = validate.String()
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validator.validate, v)
+        fn = validate.string
+        check_fail('function', self, fn, values)
+        validator = validate.String().validate
+        check_fail('class', self, fn, values)
 
 
 class TestInteger(unittest.TestCase):
 
+    type='Integer'
+
     def test_validate_pass(self):
+        self.section='pass'
         values = [
             1,
             -1,
             0,
             1.0,
             1L,
-            None,
             ]
-        for v in values:
-            print 'checking %s'%v
-            validate.integer(v)
-        validator = validate.Integer()
-        for v in values:
-            print 'checking %s'%v
-            validator.validate(v)
+        fn = validate.integer
+        check_pass('function',self, fn, values)
+        fn = validate.Integer().validate
+        check_pass('class', self, fn, values)
 
     def test_validate_fail(self):
+        self.section='pass'
         values = [
             1.01,
             'foo',
             ['a','b','c'],
             '1',
             ]
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validate.integer, v)
-        validator = validate.Integer()
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validator.validate, v)
+        fn = validate.integer
+        check_fail('function', self, fn, values)
+        validator = validate.Integer().validate
+        check_fail('class', self, fn, values)
 
 
 class TestRequired(unittest.TestCase):
@@ -83,13 +102,10 @@ class TestRequired(unittest.TestCase):
             [None],
             'None',
             ]
-        for v in values:
-            print 'checking %s'%v
-            validate.required(v)
-        validator = validate.Required()
-        for v in values:
-            print 'checking %s'%v
-            validator.validate(v)
+        fn = validate.required
+        check_pass('function',self, fn, values)
+        fn = validate.Required().validate
+        check_pass('class', self, fn, values)
 
     def test_validate_fail(self):
         values = [
@@ -97,11 +113,7 @@ class TestRequired(unittest.TestCase):
             [],
             u'',
             ]
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validate.required, v)
-        validator = validate.Required()
-        for v in values:
-            print 'checking %s'%v
-            self.assertRaises(validate.Invalid,validator.validate, v)
-
+        fn = validate.required
+        check_fail('function', self, fn, values)
+        validator = validate.Required().validate
+        check_fail('class', self, fn, values)
